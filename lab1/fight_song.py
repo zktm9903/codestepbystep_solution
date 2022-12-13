732b34019ee3b998a27d9ed5656e342fc9ffdec1
zktm9903 <zktm9903@naver.com>
2022-12-13 13:08:20 +0900

    코드 누락 수정

---
+++ lab1/fight_song.py
@@ -0,0 +1,22 @@
+def first():
+    print('Go, team, go!')
+    print('You can do it.')
+    
+def second():
+    print('You\'re the best,')
+    print('In the West.')
+    
+def fight_song():
+    first()
+    print()
+    first()
+    second()
+    first()
+    print()
+    first()
+    second()
+    first()
+    print()
+    first()
+    
+fight_song()
